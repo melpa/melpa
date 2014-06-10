3cfce4af16cab6400540d05c84491c07cb360520
Steve Purcell <steve@sanityinc.com>
2014-06-10 19:00:08 +0100

    Fix descriptive error in Commentary

--- package-build.el
+++ package-build.el
@@ -32,7 +32,7 @@
 
 ;; This file allows a curator to publish an archive of Emacs packages.
 
-;; The archive is generated from an index, which contains a list of
+;; The archive is generated from a set of recipes which describe elisp
 ;; projects and repositories from which to get them.  The term
 ;; "package" here is used to mean a specific version of a project that
 ;; is prepared for download and installation.

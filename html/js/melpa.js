0491b6bdfdb319b8e8c86aaff8c10c6e5f5a87af
Steve Purcell <steve@sanityinc.com>
2015-01-01 15:36:42 +0000

    Don't blank the site name before filling it in

--- html/js/melpa.js
+++ html/js/melpa.js
@@ -479,7 +479,7 @@
 
   jQuery(window).load(function() {
     document.title = (new melpa.archivename.controller()).archiveName();
-    jQuery(".archive-name").empty().each(function(i, e) {
+    jQuery(".archive-name").each(function(i, e) {
       // jshint unused: false
       m.module(e, melpa.archivename);
     });

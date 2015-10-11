275447974d967a735d5d2fc177278101cb1d3f90
Steve Purcell <steve@sanityinc.com>
2015-10-11 18:50:38 +1300

    Build new/modified recipes as part of the Travis check

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -12,3 +12,14 @@ echo
 "$ECUKES_EMACS" --batch --eval "(unless (ignore-errors (require 'cl-lib)) (package-refresh-contents) (package-install 'cl-lib))"
 
 cask exec ecukes
+
+if [ -n "$TRAVIS_COMMIT_RANGE" ]; then
+    CHANGED_RECIPES=$(git show --pretty=format: --name-only "$TRAVIS_COMMIT_RANGE" |grep -e '^recipes/')
+    if [ -n "$CHANGED_RECIPES" ]; then
+        for recipe_file in $CHANGED_RECIPES; do
+            recipe_name=$(echo "$recipe_file"|sed 's/^recipes\///')
+            echo "Building new/modified recipe: $recipe_name"
+            "$ECUKES_EMACS" --batch --eval "(progn (load-file \"package-build.el\")(package-build-archive '$recipe_name))"
+        done
+    fi
+fi

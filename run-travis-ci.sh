3c50cc7529f3740d204e285224013a4081f92e0e
Steve Purcell <steve@sanityinc.com>
2016-03-14 11:44:05 +1300

    Better detect changed recipes to build on Travis
    See #3214

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -14,16 +14,22 @@ echo
 
 cask exec ecukes
 
-if [ -n "$TRAVIS_COMMIT_RANGE" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo "Building recipes touched in commits $TRAVIS_COMMIT_RANGE"
-    changed_recipes=$(git show --pretty=format: --name-only "$TRAVIS_COMMIT_RANGE" |grep -e '^recipes/[a-z0-9]'|sed 's/^recipes\///'|uniq)
-    for recipe_name in $changed_recipes; do
-        if [ -f "./recipes/$recipe_name" ]; then
-            echo "----------------------------------------------------"
-            echo "Building new/modified recipe: $recipe_name"
-            "$ECUKES_EMACS" --batch --eval "(progn (load-file \"package-build.el\")(package-build-archive '$recipe_name))"
-        fi
-    done
+    changed_recipes=$(git show --pretty=format: --name-only "$TRAVIS_COMMIT_RANGE"|grep -e '^recipes/[a-z0-9]'|cut -d'|' -f1|sed 's/^recipes\///'|uniq)
+elif [ -n "$TRAVIS_PULL_REQUEST" ]; then
+    echo "Building recipes touched in pull request $TRAVIS_PULL_REQUEST on $TRAVIS_BRANCH"
+    changed_recipes=$(git diff --stat "$TRAVIS_COMMIT" "$TRAVIS_BRANCH"|grep -e '^ *recipes/[a-z0-9]'|cut -d'|' -f1|sed 's/^recipes\///'|uniq)
+else
+    changed_recipes=""
 fi
+
+for recipe_name in $changed_recipes; do
+    if [ -f "./recipes/$recipe_name" ]; then
+        echo "----------------------------------------------------"
+        echo "Building new/modified recipe: $recipe_name"
+        "$ECUKES_EMACS" --batch --eval "(progn (load-file \"package-build.el\")(package-build-archive '$recipe_name))"
+    fi
+done
 
 echo "Build successful"

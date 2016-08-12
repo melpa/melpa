5d5cfd60029bb95afd1030a95506d453bdc8e519
Steve Purcell <steve@sanityinc.com>
2016-08-12 19:29:38 +1200

    Update path to package-build in recipes/.dir-locals.el

--- recipes/.dir-locals.el
+++ recipes/.dir-locals.el
@@ -3,7 +3,7 @@
                         (file-regular-p (buffer-file-name))
                         (string-match-p "^[^.]" (buffer-file-name)))
                    (unless (featurep 'package-build)
-                     (let ((load-path (cons ".." load-path)))
+                     (let ((load-path (cons "../package-build" load-path)))
                        (require 'package-build)))
                    (package-build-minor-mode)
                    (set (make-local-variable 'package-build-working-dir) (expand-file-name "../working/"))

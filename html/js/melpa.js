3f4e8e0a4f5a4a9c368260bdb6b398648efa3ba7
Sean Allred <code@seanallred.com>
2017-05-06 22:18:05 -0500

    Show commit information if available

--- html/js/melpa.js
+++ html/js/melpa.js
@@ -40,7 +40,7 @@
   melpa.rootURL = window.location.protocol + "//" + window.location.host;
 
   melpa.Package = function(data) {
-    ["name", "description", "version", "dependencies", "source",
+    ["name", "description", "version", "dependencies", "source", "commit",
      "downloads", "fetcher", "recipeURL", "packageURL", "sourceURL", "oldNames"].map(function(p) {
       this[p] = data[p];
     }.bind(this));
@@ -150,6 +150,7 @@
       pkgs.push(new melpa.Package({
         name: name,
         version: version,
+        commit: built.props.commit,
         dependencies: deps,
         description: built.desc.replace(/\s*\[((?:source: )?\w+)\]$/, ""),
         source: recipe.fetcher,
@@ -406,7 +407,8 @@
             ]),
             m("dt", "Source"),
             m("dd", [
-              pkg.sourceURL ? m("a", {href: pkg.sourceURL}, pkg.source) : pkg.source
+              pkg.sourceURL ? m("a", {href: pkg.sourceURL}, pkg.source + pkg.commit) : pkg.source,
+              pkg.commit ? m("span.muted", " (" + pkg.commit.substring(0,6) + ")") : []
             ]),
             m("dt", "Dependencies"),
             m("dd", intersperse(_.sortBy(pkg.dependencies, 'name').map(this.depLink), " / ")),

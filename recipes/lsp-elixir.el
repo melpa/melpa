d90b8404571eb5890210fc91090db0e826f51187
Jonas Bernoulli <jonas@bernoul.li>
2019-02-01 16:44:07 +0100

    Fix name of lsp-elixir

--- recipes/lsp-elixir.el
+++ recipes/lsp-elixir.el
@@ -1,4 +1,4 @@
-(lsp-elixir.el
-  :fetcher github 
-  :repo "elixir-lsp/lsp-elixir.el" 
-  :files (:defaults "elixir-ls"))
+(lsp-elixir
+ :fetcher github
+ :repo "elixir-lsp/lsp-elixir.el"
+ :files (:defaults "elixir-ls"))

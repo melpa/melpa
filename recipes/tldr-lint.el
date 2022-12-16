b6f233e0c0d985ab3c8fe42d591f3bc2079ad293
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-16 21:49:14 +1000

    Add `tldr-lint` extension recipe

---
+++ recipes/tldr-lint.el
@@ -0,0 +1,7 @@
+(tldr-lint
+ :fetcher github
+ :url "https://github.com/tldr-pages/tldr-emacs-extension"
+ :repo "tldr-pages/tldr-emacs-extension"
+ :commit "ea76bb6d5c7c9d2d43074f656ce49cdc19ec1ca5"
+ :version-regexp "v\\(.*\\)\.\\(.*\\)"
+ :files ("*.el"))

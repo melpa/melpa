964a7e425da92a9ca3a0e71a55daaf4e0767e744
Jonas Bernoulli <jonas@bernoul.li>
2023-02-17 12:16:36 +0100

    Fix regression in .dir-locals.el
    Do not try to turn on `package-recipe-mode' when visiting the
    directory containing the recipes.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 (("recipes"
-  (nil (eval . (and (not (eq major-mode 'package-recipe-mode))
+  (nil (eval . (and buffer-file-name
+		    (not (eq major-mode 'package-recipe-mode))
 		    (or (require 'package-recipe-mode nil t)
 			(let ((load-path (cons "../package-build" load-path)))
 			  (require 'package-recipe-mode nil t)))

b9d109b8983ae29180a30b8de5158fcb77c91618
Jonas Bernoulli <jonas@bernoul.li>
2023-04-17 12:42:45 +0200

    Prepare to publish "releases" channel

--- docker/logprocessor/run.sh
+++ docker/logprocessor/run.sh
@@ -13,5 +13,8 @@ THIS_DIR=$(dirname "$0")
 # Snapshots
 "$THIS_DIR/buildstats" /mnt/store/log-snapshots /mnt/db/parquet-snapshots html-snapshots/download_counts.json
 
+# Releases
+"$THIS_DIR/buildstats" /mnt/store/log-releases /mnt/db/parquet-releases html-releases/download_counts.json
+
 echo "Sleeping"
 sleep 1800

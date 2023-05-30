b99e973e86f083e997b6e6c4b947ae6616d96192
Alan Pearce <alan@alanpearce.eu>
2023-05-30 10:27:06 +0200

    Pre-process JSON files to speed up updates.rss generation

--- docker/syncer/run.sh
+++ docker/syncer/run.sh
@@ -3,8 +3,8 @@
 MELPA_REPO=/mnt/store/melpa
 cd "${MELPA_REPO}"
 
-MELPA_CHANNEL=unstable make archive-contents json html
-MELPA_CHANNEL=stable   make archive-contents json html
+MELPA_CHANNEL=unstable make -j2 archive-contents json html
+MELPA_CHANNEL=stable   make -j2 archive-contents json html
 
 # Sync every 5 minutes.
 sleep 5m

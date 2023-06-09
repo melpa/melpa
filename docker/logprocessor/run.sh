2f51005a171b19e6375e67d4b572d8806a779d01
Alan Pearce <alan@alanpearce.eu>
2023-06-09 16:51:24 +0200

    WIP: erb

--- docker/logprocessor/run.sh
+++ docker/logprocessor/run.sh
@@ -3,14 +3,18 @@
 MELPA_REPO=/mnt/store/melpa
 cd "${MELPA_REPO}"
 
-# Unstable
-/usr/bin/python3 ${MELPA_REPO}/docker/logprocessor/process_log.py \
-  --db /mnt/db/download_log_full.db \
-  --jsondir html \
-  /mnt/store/log/melpa.access.log
+while true; do
+	# Unstable
+	/usr/bin/python3 ${MELPA_REPO}/docker/logprocessor/process_log.py \
+		--db /mnt/db/download_log_full.db \
+		--jsondir html \
+		/mnt/store/log/melpa.access.log
 
-# Stable
-/usr/bin/python3 ${MELPA_REPO}/docker/logprocessor/process_log.py \
-  --db /mnt/db/download_log_stable_full.db \
-  --jsondir html-stable \
-  /mnt/store/log-stable/melpa.access.log
+	# Stable
+	/usr/bin/python3 ${MELPA_REPO}/docker/logprocessor/process_log.py \
+		--db /mnt/db/download_log_stable_full.db \
+		--jsondir html-stable \
+		/mnt/store/log-stable/melpa.access.log
+
+	sleep 300
+done

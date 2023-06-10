094245bd7496ed1ab6f75d474eec8c86e4106886
Jonas Bernoulli <jonas@bernoul.li>
2023-06-10 16:35:05 +0200

    Fix built time reporting again

--- docker/builder/run.sh
+++ docker/builder/run.sh
@@ -11,6 +11,8 @@ LISP_CONFIG="(setq package-build-timeout-secs 600)"
 MELPA_REPO=/mnt/store/melpa
 cd "${MELPA_REPO}"
 
+BUILD_STATUS_FILE="${MELPA_REPO}/html/build-status.json"
+
 echo ">>> Pulling MELPA repository"
 MELPA_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git fetch origin
@@ -19,9 +21,8 @@ git pull origin "${MELPA_BRANCH}"
 echo
 
 record_build_status() {
-    local file="${MELPA_REPO}/html/build-status.json"
-    echo "Recording build status in $file"
-    cat <<EOF > $file
+    echo "Recording build status in $BUILD_STATUS_FILE"
+    cat <<EOF > $BUILD_STATUS_FILE
 {
   "started": $BUILD_STARTED,
   "completed": ${BUILD_COMPLETED-null},
@@ -29,15 +30,15 @@ record_build_status() {
   "next": ${BUILD_NEXT-null},
 }
 EOF
-    cat "$file"
+    cat "$BUILD_STATUS_FILE"
     # FIXME "melpa.js" expects this file in a channel-specific
     # location, but we no longer record this per channel.  For
     # now just duplicate the file.
-    cp "$file" "${MELPA_REPO}/html-stable/build-status.json"
+    cp "$BUILD_STATUS_FILE" "${MELPA_REPO}/html-stable/build-status.json"
 }
 
 # Indicate that the build is in progress
-BUILD_DURATION=$(jq ".duration" build-status.json)
+BUILD_DURATION=$(jq ".duration" ${BUILD_STATUS_FILE)
 BUILD_STARTED=$(date "+%s")
 record_build_status
 

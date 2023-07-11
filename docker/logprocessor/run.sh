4d787fbfd3b555a637cae7c5b352900ffd1f5eaa
Donald Curtis <d@milkbox.net>
2023-07-11 07:27:45 -0500

    Use `python3` for log processing.
    We are no longer using the plain python binary and should use python3 on our new server.

--- docker/logprocessor/run.sh
+++ docker/logprocessor/run.sh
@@ -4,13 +4,13 @@ MELPA_REPO=/mnt/store/melpa
 cd "${MELPA_REPO}"
 
 # Unstable
-/usr/bin/python ${MELPA_REPO}/docker/logprocessor/process_log.py \
+/usr/bin/python3 ${MELPA_REPO}/docker/logprocessor/process_log.py \
   --db /mnt/db/download_log_full.db \
   --jsondir html \
   /mnt/store/log/melpa.access.log
 
 # Stable
-/usr/bin/python ${MELPA_REPO}/docker/logprocessor/process_log.py \
+/usr/bin/python3 ${MELPA_REPO}/docker/logprocessor/process_log.py \
    --db /mnt/db/download_log_stable_full.db \
    --jsondir html-stable \
    /mnt/store/log-stable/melpa.access.log

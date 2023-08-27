917a4dc5d34977c6e02f5ed2ddb16363a2230066
Steve Purcell <steve@sanityinc.com>
2023-08-27 19:58:08 +0200

    Use the same "db" volume path as before

--- docker/logprocessor/run.sh
+++ docker/logprocessor/run.sh
@@ -5,7 +5,7 @@ THIS_DIR=$(dirname "$0")
 # TODO: switch output file name once tested & working
 
 # Unstable
-"$THIS_DIR/buildstats" /mnt/store/log /mnt/store/db/parquet > html/download_counts_new.json
+"$THIS_DIR/buildstats" /mnt/store/log /mnt/db/parquet > html/download_counts_new.json
 
 # Stable
-"$THIS_DIR/buildstats" /mnt/store/log-stable /mnt/store/db/parquet-stable > html-stable/download_counts_new.json
+"$THIS_DIR/buildstats" /mnt/store/log-stable /mnt/db/parquet-stable > html-stable/download_counts_new.json

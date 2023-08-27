b7053cb4739e440249a15fe8d0c84f41c51a392f
Steve Purcell <steve@sanityinc.com>
2023-08-27 20:03:45 +0200

    Remove defunct comments

--- docker/logprocessor/log_to_parquet.py
+++ docker/logprocessor/log_to_parquet.py
@@ -20,7 +20,6 @@ LOGREGEX = r'^(?P<ip>[\d.]+) [ -]+ \[(?P<date>[\w/: +-]+)\] ' \
 def ip2int(addr):
     return struct.unpack("!I", socket.inet_aton(addr))[0]
 
-# In Python 2.7, strptime doesn't understand "%z", so we have to work around it
 def parse_logfile():
     duckdb.execute('''CREATE TABLE IF NOT EXISTS downloads
                       ( package TEXT NOT NULL
@@ -34,7 +33,6 @@ def parse_logfile():
         match = logre.match(line)
         if match is None:
             continue
-        # Convert ips to four character strings.
         ip = ip2int(match.group('ip'))
         pkg = match.group('package')
         date = match.group('date')
@@ -43,8 +41,6 @@ def parse_logfile():
         duckdb.execute("INSERT INTO downloads VALUES (?, ?, strptime(?, '%d/%b/%Y:%H:%M:%S %z'), ?, ?)", (pkg, version, date, ip, agent))
         count += 1
 
-    # force_not_null doesn't seem to be available in this duckdb version, contrary to the docs, so we work around with nullstr
-    # to make sure empty strings aren't interpreted as nulls
     duckdb.execute("COPY downloads TO '/dev/stdout' (FORMAT PARQUET, COMPRESSION 'zstd')")
     print(f"Processed {count} downloads", file=sys.stderr)
     return count

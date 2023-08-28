c4eda0d1523aed148e8070a585622b250ca1d1f6
Steve Purcell <steve@sanityinc.com>
2023-08-28 10:04:52 +0200

    Don't redirect into download stats file, so it isn't truncated during processing

--- docker/logprocessor/run.sh
+++ docker/logprocessor/run.sh
@@ -5,7 +5,10 @@ THIS_DIR=$(dirname "$0")
 # TODO: switch output file name once tested & working
 
 # Unstable
-"$THIS_DIR/buildstats" /mnt/store/log /mnt/db/parquet > html/download_counts_new.json
+"$THIS_DIR/buildstats" /mnt/store/log /mnt/db/parquet html/download_counts.json
 
 # Stable
-"$THIS_DIR/buildstats" /mnt/store/log-stable /mnt/db/parquet-stable > html-stable/download_counts_new.json
+"$THIS_DIR/buildstats" /mnt/store/log-stable /mnt/db/parquet-stable html-stable/download_counts.json
+
+echo "Sleeping"
+sleep 1800

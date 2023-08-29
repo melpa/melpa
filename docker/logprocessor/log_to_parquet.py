0dad6b2c23db95f7302b037f0965aa0cc2e7a0ec
Steve Purcell <steve@sanityinc.com>
2023-08-29 08:37:08 +0200

    Don't store full download stats in parquet files, just counts

--- docker/logprocessor/log_to_parquet.py
+++ docker/logprocessor/log_to_parquet.py
@@ -3,52 +3,25 @@
 # Standard libraries
 import re
 import sys
-import socket
-import struct
 import tempfile
-import csv
+
+from parse import parse_logfile, TIME_FORMAT
 
 # Installed packages
 import duckdb
 
-LOGREGEX = r'^(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) [ -]+ \[(?P<date>[\w/: +-]+)\] ' \
-           r'"GET /+packages/+(?P<package>[^ ]+)-(?P<version>[0-9.]+).(?:el|tar) ' \
-           r'HTTP/\d.\d" 200 \d+ "[^"]*?" "(?P<agent>[^"]*)'
-
-def ip2int(addr):
-    return struct.unpack("!I", socket.inet_aton(addr))[0]
-
-def parse_logfile():
-    duckdb.execute('''CREATE TEMP TABLE IF NOT EXISTS downloads
-                      ( package TEXT NOT NULL
-                      , version TEXT NOT NULL
-                      , date TIMESTAMPTZ NOT NULL
-                      , ip UINTEGER NOT NULL
-                      , agent TEXT NOT NULL)''')
-    logre = re.compile(LOGREGEX)
+def main():
     count = 0
-    with tempfile.NamedTemporaryFile('w', suffix='.csv') as csvfile:
-        rows = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        rows.writerow(["package", "version", "date", "ip", "agent"])
-        for line in sys.stdin:
-            match = logre.match(line)
-            if match is None:
-                continue
-            ip = ip2int(match.group('ip'))
-            package = match.group('package')
-            date = match.group('date')
-            version = match.group('version')
-            agent = match.group('agent')
-            rows.writerow([package, version, date, ip, agent])
-            count += 1
-
-        csvfile.flush()
-        duckdb.execute(f"INSERT INTO downloads SELECT package, version, strptime(date, '%d/%b/%Y:%H:%M:%S %z'), ip, agent from read_csv_auto('{csvfile.name}', header=true, nullstr='&&&&&')")
-
-    duckdb.execute("COPY downloads TO '/dev/stdout' (FORMAT PARQUET, COMPRESSION 'zstd')")
+    stats = {}
+    for row in parse_logfile(sys.stdin):
+        stats[row['package']] = stats.get(row['package'], 0) + 1
+        count += 1
+    duckdb.execute('CREATE TEMP TABLE stats (package TEXT NOT NULL, count UINTEGER NOT NULL)')
+    duckdb.executemany('INSERT INTO stats VALUES (?, ?)', stats.items())
+    duckdb.execute("COPY stats TO '/dev/stdout' (FORMAT PARQUET, COMPRESSION 'zstd')")
     print(f"Processed {count} downloads", file=sys.stderr)
     return count
 
 
 if __name__ == '__main__':
-    parse_logfile()
+    main()

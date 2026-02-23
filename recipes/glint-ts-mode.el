70fa3528b131418bb95a84376ad11f7c8adc641c
file not found <voodoologic@users.noreply.github.com>
2026-02-23 12:28:17 -0800

    add build-depends

--- recipes/glint-ts-mode.el
+++ recipes/glint-ts-mode.el
@@ -1,2 +1,4 @@
-(glint-ts-mode :repo "overcast-software/glint-ts-mode"
-               :files ("glint-ts-mode.el" "lsp-glint.el" "README.md"))
+(glint-ts-mode
+ :repo "overcast-software/glint-ts-mode"
+ :files ("glint-ts-mode.el" "lsp-glint.el" "README.md")
+ :build-depends ("lsp-mode" "typescript-ts-mode"))

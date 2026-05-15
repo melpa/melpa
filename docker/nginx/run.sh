4e536cc6d431da50e43dd60ae25a8809b6ca862b
Jonas Bernoulli <jonas@bernoul.li>
2026-05-15 14:26:44 +0200

    Prepare to publish "snapshots" channel

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -43,8 +43,10 @@ while true; do
         --expand \
         -d melpa.org \
         -d stable.melpa.org \
+        -d snapshots.melpa.org \
         -d test.melpa.org \
         -d stable-test.melpa.org \
+        -d snapshots-test.melpa.org \
         -d www.melpa.org
 
     echo "restarting nginx..."
--- docker/syncer/run.sh
+++ docker/syncer/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-CHANNEL=unstable make archive-contents json html
-CHANNEL=stable   make archive-contents json html
+CHANNEL=unstable  make archive-contents json html
+CHANNEL=stable    make archive-contents json html
+CHANNEL=snapshots make archive-contents json html
 
 # Sync every 5 minutes.
 sleep 5m
